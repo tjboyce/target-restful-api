e4f99425f3eedaf591dea4425e7bade037116f45
Todd Boyce Jr <tjboyce@me.com>
2019-07-17 17:35:33 -0500

    add body-parser

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const restfulRouter = require('./routes/restful.router');
+const bodyParser = require('body-parser');
 
 /** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+/**-----------SERVE STATIC FILES-------- */
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
